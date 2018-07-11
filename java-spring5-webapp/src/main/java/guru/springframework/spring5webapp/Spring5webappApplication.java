ce98dc6594ad78748883bb9eaaa65996e8b681d6
Yaniv M <yaniv_menuhin@bmc.com>
2018-07-11 15:35:43 +0300

    aaa

--- java-spring5-webapp/src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ java-spring5-webapp/src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -1,4 +1,4 @@
-package q.ymen.spring5webapp;
+package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
 }
--- java-spring5-webapp/src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
+++ java-spring5-webapp/src/test/java/guru/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -1,4 +1,4 @@
-package q.ymen.spring5webapp;
+package guru.springframework.spring5webapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
