46cd6f426ec1bd67fffe3a8f071ac1b2cc6165a8
yanivm <yaniv.menuhin@gmail.com>
2018-10-15 13:14:19 +0300

    Additional projects

--- spring-rabbitmq/src/main/java/qs/rabbitmq/RabbitmqApplication.java
+++ spring-rabbitmq/src/main/java/qs/rabbitmq/RabbitmqApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import qs.rabbitmq.listener.ProductMessageListener;
 
 @SpringBootApplication
+@
 public class RabbitmqApplication {
 
     // Configuration ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
